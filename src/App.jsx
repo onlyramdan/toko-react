c07c9a274de440732a6e9778613f978777f21fb0
Ramdan <77191112+onlyramdan@users.noreply.github.com>
2024-02-13 03:38:38 +0700

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./assets/css/App.css";
+
+export default function App() {
+  return <h1 className="text-xl font-light underline">Set Up pertama</h1>;
+}
